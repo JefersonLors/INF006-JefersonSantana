7b2e7603cec12cd82bef68f15fd3b6d4bb809a9a
Jeferson Lors <69722947+JefersonLors@users.noreply.github.com>
2022-05-23 12:24:52 +0000

    TRABALHO FINAL INICIO

--- Trabalho_Final/main.c
+++ Trabalho_Final/main.c
@@ -1,8 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+unsigned MENU_PRINCIPAL( void );
 
 int main( ){
-    printf( "\tBOLSA DE VALORES\n\n" );
+    enum OPCOES { SAIR = 0, COMPRAR = 1, VENDER = 2, CARTEIRA = 3, PAPEIS = 4 };
+    int resposta;
+    
+    do{ resposta = MENU_PRINCIPAL( ); 
+        
+            
+    
+    }while( resposta != SAIR );
+    
     
     return 0;
 }
+ unsigned MENU_PRINCIPAL(  ){
+    int resposta;
+    printf( "=========================================\n"
+            "\t\t\tBOLSO DE VALORES\n\n" 
+            "[1] COMPRAR\n"
+            "[2] VENDER\n"
+            "[3] CARTEIRA\n"
+            "[4] PAPEIS\n"
+            "[0] SAIR\n\n"
+            "[ ] <- " 
+        );
+    scanf( "%d", &resposta ); getchar( ); 
+    while( resposta < 0 || resposta > 4 ){
+        puts( "\t\t\tOPÇÃO INVÁLIDA!\n\n"
+              "[ ] <-  " ); 
+        scanf( "%d", &resposta ); getchar( );}
+    return resposta;
+ }
