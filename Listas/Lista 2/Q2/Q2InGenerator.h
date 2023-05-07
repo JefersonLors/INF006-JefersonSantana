60a13074008a92b59a0a4d7a4abcd356cea2309e
Jeferson Lors <69722947+JefersonLors@users.noreply.github.com>
2023-05-07 16:53:52 -0300

    Add files via upload

---
+++ Listas/Lista 2/Q2/Q2InGenerator.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef Q2InGenerator_h
+#define Q2InGenerator_h
+
+#define NAME_LIST_SIZE 52
+#define _MAX_SIZE_LINE 255
+#define MAX_QTY_NAME 30
+#define MAX_QTY_LINE 60000
+#define MIN_QTY_LINE 10000
+
+typedef struct _string
+{
+  char *line;
+  struct _string *next;
+} _str;
+
+extern const char *R1Q2_file_in;
+extern const char *names[NAME_LIST_SIZE];
+
+void L1Q2_in_generator();
+_str *create_name_list();
+char *create_line(int qty);
+
+bool write_in_file(_str *start);
+void delete_line_list(_str **start);
+
+#endif
