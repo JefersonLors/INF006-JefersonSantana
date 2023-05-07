3ad909c3eefdfd19d28996e654f30e4915c6d25d
Jeferson Lors <lors.jeferson@gmail.com>
2023-05-07 16:54:51 -0300

    wip(Q2): gerador de entrada pronto

---
+++ Listas/Lista 2/Q2/R1Q2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include "R1Q2.h"
+#include "Q2InGenerator.h"
+
+int main( ){
+  L1Q2_in_generator();
+  
+  puts("fim");
+  return EXIT_SUCCESS;
+}
---
+++ Listas/Lista 2/Q2/R1Q2.h
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#ifndef R1Q2_h
+#define R1Q2_h
+
+
+
+#endif
