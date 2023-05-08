7f7d66efced8d452215bc818f9e58e4c5fb25c0d
Jeferson Lors <lors.jeferson@gmail.com>
2023-05-08 11:20:33 -0300

    wip(Q2): recuperação das linhas do arquivo done

--- Listas/Lista 2/Q2/R1Q2.c
+++ Listas/Lista 2/Q2/R1Q2.c
@@ -6,8 +6,167 @@
 #include "Q2InGenerator.h"
 
 int main( ){
-  L1Q2_in_generator();
-  
+  // L1Q2_in_generator();
+  str *firstStr = get_lines_from_file();
+  strBase *firstStrBase = create_str_matrix(firstStr);
+
+
   puts("fim");
   return EXIT_SUCCESS;
+}
+strBase *create_str_matrix(str *firstLine)
+{
+  str *firstStrList = firstLine;
+
+  strBase *firstStrBase = NULL;
+  strBase *actStrBase = NULL;
+  strBase *newStrBase = NULL;
+  strBase *lastStrBase = NULL;
+
+  if (firstStrList)
+  {
+    newStrBase = (strBase*)malloc(sizeof(strBase));
+    newStrBase->content = break_lines_in_str_list(firstStrList);
+    newStrBase->next = NULL;
+
+    firstStrBase = newStrBase;
+    lastStrBase = newStrBase;
+
+    firstStrList = firstStrList->next;
+    while (firstStrList)
+    {
+      newStrBase = (strBase*)malloc(sizeof(strBase));
+      newStrBase->content = break_lines_in_str_list(firstStrList);
+      newStrBase->next = NULL;
+
+      actStrBase = lastStrBase;
+      actStrBase->next = newStrBase;
+      lastStrBase = newStrBase;
+
+      firstStrList = firstStrList->next;
+    }
+  }
+  return firstStrBase;
+}
+str *break_lines_in_str_list(str *line)
+{
+  str *firstStrList = NULL;
+  str *actStrList = NULL;
+  str *lastStrList = NULL;
+  str *newStrList = NULL;
+
+  char *strList;
+
+  strList = strtok(line->line, KEY_WORD);
+
+  if (strList)
+  {
+    if (!isEmpty(strList))
+    {
+      newStrList = (str*)malloc(sizeof(str));
+      newStrList->line = (char*)malloc(sizeof(char) * strlen(strList));
+      strcpy(newStrList->line, strList);
+      newStrList->next = NULL;
+
+      firstStrList = newStrList;
+      lastStrList = newStrList;
+    }
+    else
+    {
+      strList = strtok(NULL, KEY_WORD);
+      while (isEmpty(strList) && strList)
+      {
+        strList = strtok(NULL, KEY_WORD);
+      }
+      if (strList)
+      {
+        newStrList = (str*)malloc(sizeof(str));
+        newStrList->line = (char*)malloc(sizeof(char) * strlen(strList));
+        strcpy(newStrList->line, strList);
+        newStrList->next = NULL;
+
+        firstStrList = newStrList;
+        lastStrList = newStrList;
+      }
+    }
+    if (firstStrList)
+    {
+      strList = strtok(NULL, KEY_WORD);
+
+      while (strList != NULL)
+      {
+        if (!isEmpty(strList))
+        {
+          newStrList = (str*)malloc(sizeof(str));
+          newStrList->line = (char*)malloc(sizeof(char) * strlen(strList));
+
+          strcpy(newStrList->line, strList);
+
+          newStrList->next = NULL;
+          actStrList = lastStrList;
+          actStrList->next = newStrList;
+          lastStrList = newStrList;
+        }
+        strList = strtok(NULL, KEY_WORD);
+      }
+    }
+  }
+  return firstStrList;
+}
+str *get_lines_from_file()
+{
+  FILE *fileInPtr = fopen(R1Q2_file_in_path, "r");
+
+  str *firstStr = NULL;
+  str *lastStr = NULL;
+  str *actStr = NULL;
+  str *newStr = NULL;
+
+  char line[MAX_SIZE_LINE];
+
+  if (fileInPtr)
+  {
+    if (fgets(line, MAX_SIZE_LINE, fileInPtr))
+    {
+      line[strlen(line) - 1] = '\0';
+      newStr = (str *)malloc(sizeof(str));
+      newStr->line = (char *)malloc(sizeof(char) * MAX_SIZE_LINE);
+      strcpy(newStr->line, line);
+      newStr->next = NULL;
+
+      firstStr = newStr;
+      lastStr = newStr;
+
+      while (fgets(line, MAX_SIZE_LINE, fileInPtr))
+      {
+        line[strlen(line) - 1] = '\0';
+        newStr = (str *)malloc(sizeof(str));
+        newStr->line = (char *)malloc(sizeof(char) * MAX_SIZE_LINE);
+        strcpy(newStr->line, line);
+        newStr->next = NULL;
+
+        actStr = lastStr;
+        actStr->next = newStr;
+        lastStr = newStr;
+      }
+    }
+  }
+  return firstStr;
+}
+bool isEmpty(char *string)
+{
+  int i = 0;
+
+  if (string)
+  {
+    while (string[i] != '\0')
+    {
+      if (string[i] != ' ')
+      {
+        return false;
+      }
+      i++;
+    }
+  }
+  return true;
 }
--- Listas/Lista 2/Q2/R1Q2.h
+++ Listas/Lista 2/Q2/R1Q2.h
@@ -4,6 +4,45 @@
 #ifndef R1Q2_h
 #define R1Q2_h
 
+#define MAX_SIZE_LINE 255
+#define KEY_WORD " "
 
+enum operation{
+  NONE = 0,
+  PUSH,
+  POP,
+};
+
+typedef struct name
+{
+  char *content;
+  unsigned short operation;
+  struct name *next;
+} name;
+
+typedef struct nameBase{
+  struct name *list;
+  struct nameBase *next; 
+} nameBase;
+
+typedef struct string
+{
+  char *line;
+  struct string *next;
+} str;
+
+typedef struct stringBase
+{
+  struct string *content;
+  struct stringBase *next;
+} strBase;
+
+str *get_lines_from_file();
+str *break_lines_in_str_list(str *line);
+strBase *create_str_matrix(str *firstLine);
+bool isEmpty(char *string);
+
+const char R1Q2_file_in_path[] = "L1Q2.in";
+const char R1Q2_file_out_path[] = "L1Q2.out";
 
 #endif
