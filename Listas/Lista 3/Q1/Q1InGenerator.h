33b142f918abd8d925ca8100dbb293f1ab07c453
Jeferson Lors <lors.jeferson@gmail.com>
2023-06-10 18:01:41 -0300

    wip(Q1): gerador de entrada feito

---
+++ Listas/Lista 3/Q1/Q1InGenerator.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#ifndef Q1InGenerator_h
+#define Q1InGenerator_h
+
+#define _MAX_SIZE_LINE 800
+#define MAX_QTY_NUMBERS 100
+#define MAX_QTY_LINE 60000
+#define MIN_QTY_LINE 10000
+
+typedef struct _string{
+  char *line;
+  struct _string *next;
+}_str;
+
+extern const char *R2Q1_file_in; 
+
+void L2Q1_in_generator();
+_str *create_line_list();
+char *generate_string_interger_list();
+bool toNegative();
+bool write_in_file(_str *start);
+void delete_line_list(_str **start);
+
+#endif
