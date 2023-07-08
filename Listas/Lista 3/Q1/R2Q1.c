7aa334be5ff808f5289ffabae1b8923d0d5a3190
Jeferson Lors <lors.jeferson@gmail.com>
2023-07-08 20:04:07 -0300

    wip(Q2): finalizada

--- Listas/Lista 3/Q1/R2Q1.c
+++ Listas/Lista 3/Q1/R2Q1.c
@@ -7,11 +7,251 @@ int main()
   // L2Q1_in_generator();
   str *firstStr = get_lines_from_file();
   numbers *firstNumber = create_numbers_list(firstStr);
-  show_lines_like_numbers_list(firstNumber);
-
-  puts("end execution");
+  delete_str_list(&firstStr);
+  lineResult *firstLineResult = create_line_result_list(firstNumber);
+  delete_numbers_list(&firstNumber);
+  write_result_in_file(firstLineResult);
+  delete_line_result_list(&firstLineResult);
+  puts("\nend execution");
   return EXIT_SUCCESS;
 }
+void write_result_in_file(lineResult *firstLineResult)
+{
+  FILE *fileOutPtr = fopen(R2Q1_file_out_path, "w");
+
+  while (firstLineResult)
+  {
+    int i = 0;
+
+    while (i < firstLineResult->hQty)
+    {
+      fprintf(fileOutPtr, "%d ", firstLineResult->heights[i]);
+      i++;
+    }
+    fprintf(fileOutPtr, "max %d alt %d pred ", firstLineResult->max, firstLineResult->maxH);
+    if (firstLineResult->maxPred != EMPTY)
+    {
+      fprintf(fileOutPtr, "%d\n", firstLineResult->maxPred);
+    }else{
+      fprintf(fileOutPtr, "%s\n", "NaN");
+    }
+
+    firstLineResult = firstLineResult->next;
+  }
+  fclose(fileOutPtr);
+}
+void delete_numbers_list(numbers **firstNumbers)
+{
+  numbers *curr = *firstNumbers;
+  numbers *prev = NULL;
+
+  while (curr)
+  {
+    prev = curr;
+    curr = curr->next;
+    free(prev);
+    prev = NULL;
+  }
+  *firstNumbers = NULL;
+}
+void delete_line_result_list(lineResult **firstlineResult)
+{
+  lineResult *curr = *firstlineResult;
+  lineResult *prev = NULL;
+
+  while (curr)
+  {
+    prev = curr;
+    curr = curr->next;
+    free(prev);
+    prev = NULL;
+  }
+  *firstlineResult = NULL;
+}
+lineResult *create_line_result_list(numbers *values)
+{
+  lineResult *first = NULL;
+
+  if (values)
+  {
+    lineResult *new = new_line_result();
+    lineResult *curr = NULL;
+    lineResult *last = NULL;
+
+    int *heights = (int *)malloc(sizeof(int) * values->keyQTY);
+
+    node *tree = create_tree(values, heights);
+    new->heights = heights;
+    new->hQty = values->keyQTY;
+    node *max = maximum(tree);
+    new->max = max->key;
+    node *pred = predecessor(max);
+    new->maxPred = pred->key;
+    new->maxH = calculate_height(max);
+
+    new->next = NULL;
+
+    first = new;
+    last = new;
+
+    values = values->next;
+
+    int line = 2;
+    while (values)
+    {
+      lineResult *new = new_line_result();
+      int *heights = (int *)malloc(sizeof(int) * values->keyQTY);
+      node *tree = create_tree(values, heights);
+      new->heights = heights;
+      new->hQty = values->keyQTY;
+      node *max = maximum(tree);
+      new->max = max->key;
+      node *pred = predecessor(max);
+      new->maxPred = (pred ? pred->key : EMPTY);
+      new->maxH = calculate_height(max);
+
+      new->next = NULL;
+
+      curr = last;
+      curr->next = new;
+      last = new;
+      line++;
+      values = values->next;
+    }
+  }
+  return first;
+}
+lineResult *new_line_result()
+{
+  lineResult *new = (lineResult *)malloc(sizeof(lineResult));
+  new->heights = NULL;
+  new->hQty = -1;
+  new->max = -1;
+  new->maxH = -1;
+  new->maxPred = -1;
+  new->next = NULL;
+  return new;
+}
+lineResult *create_line_result()
+{
+  lineResult *new = (lineResult *)malloc(sizeof(lineResult));
+  new->heights = NULL;
+  new->hQty = -1;
+  new->max = -1;
+  new->maxH = -1;
+  new->maxPred = -1;
+  new->next = NULL;
+
+  return new;
+}
+node *create_tree(numbers *values, int *heights)
+{
+  node *root = NULL;
+
+  int i = 0;
+  while (i < values->keyQTY)
+  {
+    heights[i] = insert_node(&root, create_node(values->keys[i]));
+    i++;
+  }
+  return root;
+}
+int insert_node(node **root, node *newNode)
+{
+  if (!(*root))
+  {
+    newNode->height = calculate_height(newNode);
+    (*root) = newNode;
+    return (*root)->height;
+  }
+  else
+  {
+    if ((*root)->key > newNode->key)
+    {
+      if ((*root)->left)
+      {
+        insert_node(&(*root)->left, newNode);
+      }
+      else
+      {
+        (*root)->left = newNode;
+        newNode->p = (*root);
+        newNode->height = calculate_height(newNode);
+        return newNode->height;
+      }
+    }
+    else if ((*root)->key <= newNode->key)
+    {
+      if ((*root)->right)
+      {
+        insert_node(&(*root)->right, newNode);
+      }
+      else
+      {
+        (*root)->right = newNode;
+        newNode->p = (*root);
+        newNode->height = calculate_height(newNode);
+        return newNode->height;
+      }
+    }
+  }
+}
+int calculate_height(node *subRoot)
+{
+  int h = 0;
+
+  while (subRoot->p)
+  {
+    h++;
+    subRoot = subRoot->p;
+  }
+  return h;
+}
+node *predecessor(node *root)
+{
+  node *parent;
+
+  if (root->left)
+  {
+    return maximum(root->left);
+  }
+  parent = root->p;
+
+  while (parent && root == parent->left)
+  {
+    root = parent;
+    parent = parent->p;
+  }
+  return parent;
+}
+node *maximum(node *root)
+{
+  while (root->right)
+  {
+    root = root->right;
+  }
+  return root;
+}
+void show_in_order(node *root)
+{
+  if (root)
+  {
+    show_in_order(root->left);
+    printf("%d -> [%d]\t", root->key, root->height);
+    show_in_order(root->right);
+  }
+}
+node *create_node(int value)
+{
+  node *new = (node *)malloc(sizeof(node));
+  new->key = value;
+  new->height = -1;
+  new->p = NULL;
+  new->left = NULL;
+  new->right = NULL;
+}
+
+// SEÇÃO DE RECUPERAÇÃO DE DADOS DO ARQUIVO DE ENTRADA
 str *get_lines_from_file()
 {
   FILE *fileInPtr = fopen(R2Q1_file_in_path, "r");
@@ -85,14 +325,16 @@ numbers *create_numbers_list(str *firstsStrValues)
 
   currStr = currStr->next;
 
+  int line = 2;
   while (currStr)
   {
     firstStrKey = break_line_in_str_list(currStr->content);
-
     str *currStrKey = firstStrKey;
 
     newN = new_numbers();
+
     newN->keyQTY = get_size_str_linked_list(currStrKey);
+
     newN->keys = new_int_vector(newN->keyQTY);
     newN->next = NULL;
 
@@ -110,7 +352,7 @@ numbers *create_numbers_list(str *firstsStrValues)
     currN = lastN;
     currN->next = newN;
     lastN = newN;
-
+    line++;
     currStr = currStr->next;
   }
   return firstN;
@@ -125,6 +367,7 @@ str *break_line_in_str_list(char *line)
   char *middleWhere = NULL;
 
   middleWhere = strtok(line, " ");
+
   int sizeStr = strlen(middleWhere);
 
   if (sizeStr)
--- Listas/Lista 3/Q1/R2Q1.h
+++ Listas/Lista 3/Q1/R2Q1.h
@@ -5,6 +5,7 @@
 #define R2Q1_H
 
 #define MAX_SIZE_LINE 5000
+#define EMPTY -3023348
 
 typedef struct string
 {
@@ -28,16 +29,46 @@ typedef struct node
   struct node *right;
 } node;
 
+typedef struct lineResult
+{
+  int hQty;
+  int *heights;
+  int max;
+  int maxH;
+  int maxPred;
+  struct lineResult *next;
+} lineResult;
+
 const char R2Q1_file_in_path[] = "L2Q1.in";
 const char R2Q1_file_out_path[] = "L2Q1.out";
 
 str *get_lines_from_file();
 str *new_str();
 str *break_line_in_str_list(char *line);
+
 numbers *new_numbers();
 numbers *create_numbers_list(str *firstStr);
 
+node *create_tree(numbers *values, int *heights);
+node *create_node(int value);
+node *maximum(node *root);
+node *predecessor(node *root);
+
+lineResult *create_line_result_list(numbers *values);
+lineResult *create_line_result();
+lineResult *new_line_result();
+
+void write_result_in_file(lineResult *firstLineResult);
+
+void delete_line_result_list(lineResult **firstlineResult);
 void delete_str_list(str **firstStr);
+void delete_numbers_list(numbers **firstNumber);
+
+int calculate_height(node *subRoot);
+
+int insert_node(node **root, node *newnode);
+void show_in_order(node *root);
+
 void show_lines_content(str *firstStr);
 void show_lines_like_numbers_list(numbers *firstNumbers);
 
